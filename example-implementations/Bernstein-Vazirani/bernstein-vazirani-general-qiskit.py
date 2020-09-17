a82888b8479e71d2c3549c430cd454872e475e23
Marie Salm <github@mielie.de>
2020-09-17 12:31:27 +0200

    Create bernstein-vazirani-general-qiskit.py

---
+++ example-implementations/Bernstein-Vazirani/bernstein-vazirani-general-qiskit.py
@@ -0,0 +1,40 @@
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+
+# https://github.com/Qiskit/qiskit-community-tutorials/blob/master/algorithms/bernstein_vazirani.ipynb
+# https://doi.org/10.1073/pnas.1618020114
+
+def get_circuit(**kwargs):
+    number_of_qubits = kwargs["number_of_qubits"]
+    s = kwargs["s"]
+    s = s % 2 ** (number_of_qubits)  # a = a mod 2^(number_of_qubits)
+    print(s)
+
+    qr = QuantumRegister(number_of_qubits + 1)
+    cr = ClassicalRegister(number_of_qubits + 1)
+
+    qc = QuantumCircuit(qr, cr)
+
+    # hadamard gates
+    for i in range(number_of_qubits + 1):
+        qc.h(qr[i])
+
+    qc.barrier()
+
+    # inner product oracle
+    for i in range(number_of_qubits):
+        if (s & (1 << i)):  # if bin(s)[i] = 1 then use cnot with ancilla
+            qc.cx(qr[i], qr[number_of_qubits])
+
+    qc.barrier()
+
+    # hadamard gates
+    for i in range(number_of_qubits + 1):
+        qc.h(qr[i])
+
+    # measurement
+    qc.barrier(qr)
+
+    for i in range(number_of_qubits):
+        qc.measure(qr[i], cr[i])
+
+    return qc
